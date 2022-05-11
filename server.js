ea742735fd553de18f47cc12faa5b78c76ed0cdc
Noah8863 <stacknoah99@gmail.com>
2022-05-11 17:06:21 -0600

    set up first question/action to ask the user

--- server.js
+++ server.js
@@ -12,3 +12,22 @@ app.use(express.urlencoded({extended: true}))
 // static middleware
 app.use(express.static('./public'));
 
+const actions = ['View All Departments', 'Add Departments', 'View all Employees', 'Add New Employee', 'Update Employee Role', 'Add Role', 'Quit']
+const question = [
+    {
+        type: 'list',
+        name: 'actions',
+        message: 'What would you like to do? (Select with the up and down keys)',
+        choices: actions
+    }
+]
+
+const init = () => {
+    inquirer
+        .prompt(question)
+        .then((responses) => {
+            console.log(responses)
+        })
+}
+
+init();
