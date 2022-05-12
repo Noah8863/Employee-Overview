cdb2c87b58182590e9efbdee1642daea2039adb5
Noah8863 <stacknoah99@gmail.com>
2022-05-11 18:03:38 -0600

    installed mysql package and include it into server.js

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 // Packages that are used in the program
 const express = require('express');
-const inquirer = require('inquirer')
-const fs = require('fs')
+const inquirer = require('inquirer');
 const cTable = require('console.table');
+const mysql = require('mysql2');
 
 const app = express();
 
-
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
-// static middleware
+//Express Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(express.static('./public'));
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'employee_db'
+    }, 
+    console.log(`Connected to the employee_db database`)
+)
 
 const actions = ['View All Departments', 'Add Departments', 'View all Employees', 'Add New Employee', 'Update Employee Role', 'Add Role', 'Quit']
 const question = [
