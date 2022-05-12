dad46825dad8aca8f93de20ebd96a92eaf4f6233
Noah8863 <stacknoah99@gmail.com>
2022-05-11 18:25:02 -0600

    finished adding all the questions needed in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
-const mysql = require('mysql2');
+//const mysql = require('mysql2');
 
 const app = express();
 
@@ -11,16 +11,21 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('./public'));
 
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'employee_db'
-    }, 
-    console.log(`Connected to the employee_db database`)
-)
+//const db = mysql.createConnection(
+  //  {
+    //    host: 'localhost',
+      //  user: 'root',
+        //password: '',
+        //database: 'employee_db'
+    //}, 
+    //console.log(`Connected to the employee_db database`)
+//)
 
+//Arrays for the departments and roles which can be updated
+let listOfDepartments = ['Testing1', 'Testing2', 'Testing3']
+let listOfRoles = ['Testing1', 'Testing2', 'Testing3']
+
+//Questions for the user to update and view the database
 const actions = ['View All Departments', 'Add Departments', 'View all Employees', 'Add New Employee', 'Update Employee Role', 'Add Role', 'Quit']
 const question = [
     {
@@ -31,9 +36,68 @@ const question = [
     }
 ]
 
+
+//Questions for adding Departments
+const addDepartment = [
+    {
+        type: 'input',
+        name: 'addDepartment',
+        message: 'What would you like to call the new department?'
+    }
+]
+
+
+//Questions for adding roles
+const addRole = [
+    {
+        type: 'input',
+        name: 'roleName',
+        message: 'What role would you like to add?'
+    },
+    {
+        type: 'input',
+        name: 'roleSalary',
+        message: `How much salary does this role get?`
+    },
+    {
+        type: 'list',
+        name: 'roleDepartment',
+        message: 'Which Department would you like to add this role to?',
+        choices: listOfDepartments
+    }
+]
+
+
+//Questions for adding Employee
+const addEmployee = [
+    {
+        type: 'input',
+        name: 'firstName',
+        message: 'What is the Employees first name?',
+    },
+    {
+        type: 'input',
+        name: 'lastName',
+        message: 'What is the Employees last name?'
+    },
+    {
+        type: 'list',
+        name: 'employeeTitle',
+        message: 'What is the Employees title?',
+        choices: listOfRoles
+    },
+    {
+        // Salary will be automatically updated based on the role
+        //Using this as a test question
+        type: 'input',
+        name: 'employeeSalary',
+        message: 'What is the Employees salary?'
+    }
+]
+
 const init = () => {
     inquirer
-        .prompt(question)
+        .prompt(addEmployee)
         .then((responses) => {
             console.log(responses)
         })
